af43b4354dcf6b4363100867306ce9ab0fe5fcc3
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-23 15:47:19 -0400

    designed database schema

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL
+);
+
