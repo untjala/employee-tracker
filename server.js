814d2e92987c866cedac0ca5e4d6c14fbf2a0d83
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-28 19:04:59 -0400

    update code written, project 80% functional

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const startPrompts = () => {
       'Add a department',
       'Add a role',
       'Add an employee',
-      'Update an employee role',
+      'Update employee role',
       'Quit',
     ]
   })
@@ -39,13 +39,13 @@ const startPrompts = () => {
       if (answer.startOptions === 'Update employee role') { updateRole(); }
       if (answer.startOptions === 'Quit') { db.end(); }
     })
-}
+};
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 const viewEmployees = () => {
   db.query('SELECT * FROM employee',
-    function (err, results) {
-      if (err) return console.error(err);
+    function (error, results) {
+      if (error) return console.error(error);
       console.table(results);
       startPrompts();
     });
@@ -54,8 +54,8 @@ const viewEmployees = () => {
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 const viewRoles = () => {
   db.query('SELECT * FROM role',
-    function (err, results) {
-      if (err) return console.error(err);
+    function (error, results) {
+      if (error) return console.error(error);
       console.table(results);
       startPrompts();
     });
@@ -64,13 +64,14 @@ const viewRoles = () => {
 // THEN I am presented with a formatted table showing department names and department ids
 const viewDepartments = () => {
   db.query('SELECT * FROM department',
-    function (err, results) {
-      if (err) return console.error(err);
+    function (error, results) {
+      if (error) return console.error(error);
       console.table(results);
       startPrompts();
     });
 }
 
+//Function to add a new employee
 const addEmployee = () => {
   console.log('Add employee')
   const query =
@@ -115,8 +116,8 @@ const addEmpPropmt = (seletRole) => {
           last_name: answer.newLast,
           role_id: answer.newRole
         },
-        function (err, results) {
-          if (err) throw err;
+        function (error, results) {
+          if (error) throw error;
           console.table(results);
           console.log('Added! All set!');
 
@@ -124,11 +125,11 @@ const addEmpPropmt = (seletRole) => {
         });
     });
 }
-
+//Function to add a new role
 const addRole = () => {
   console.log('Okay! What role would you like to add?')
   const query =
-  //Collabrated with a classmate to find query solution
+    //Collabrated with a classmate to find query solution
     `SELECT d.id, d.name, r.salary
     FROM employee e
     JOIN role r
@@ -165,39 +166,120 @@ const addRole = () => {
         choices: departmentChoice
       },
     ])
+      .then(function (answer) {
+        console.table(answer);
+        const query =
+          `INSERT INTO role SET ?`
+        db.query(query,
+          {
+            title: answer.newTitle,
+            salary: answer.newSalary,
+            department_id: answer.departmentId
+          },
+          function (error, results) {
+            if (error) throw error;
+            console.table(results);
+            console.log('Added! All set!');
+
+            startPrompts();
+          });
+      });
+  }
+}
+
+//Function to add a new department
+const addDepartment = () => {
+  console.log('Add a new department!')
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: 'What is the department\'s name?'
+    },
+  ])
     .then(function (answer) {
       console.table(answer);
       const query =
-        `INSERT INTO role SET ?`
+        `INSERT INTO department SET ?`
       db.query(query,
         {
-          title: answer.newTitle,
-          salary: answer.newSalary,
-          department_id: answer.departmentId
+          name: answer.departmentName
         },
-        function (err, results) {
-          if (err) throw err;
+        function (error, results) {
+          if (error) throw error;
           console.table(results);
           console.log('Added! All set!');
 
           startPrompts();
         });
     });
-  }
 }
-//   }
-//   const updateEmployee = () => {
+//Function to update a pre-existing employee's role
+const updateRole = () => {
+  console.log('Okay! What role would you like to update?')
+  const query =
+    `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS DEPARTMENT, r.salary
+    FROM employee e
+    JOIN role r
+    ON e.role_id = r.id
+    JOIN department d 
+    ON d.id = r.department_id
+    JOIN employee m
+    ON m.id = e.manager_id`
 
-//   }
-//   const updateRole = () => {
+  db.query(query, function (error, result) {
+    if (error) throw error;
+    const updatedEmp = result.map(({ id, first_name, last_name }) => ({
+      value: id, name: `${first_name} ${last_name}`
+    }));
+    console.table(result)
+    newEmp(updatedEmp);
+  });
+}
+const newEmp = (updatedEmp) => {
+  const query = `SELECT r.id, r.title, r.salary FROM role r`
+  
+  db.query(query, function (error, result) {
+    if (error) throw error;
 
-//   }
+    selectRole = result.map(({id, title, salary}) => ({
+      value: id, title: `${title}`, salary: `${salary}`
+    }));
+    console.table(result)
+
+    updatedEmpPrompt(updatedEmp, selectRole);
+  })
+};
+const updatedEmpPrompt = (updatedEmp, selectRole) => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'empId',
+      message: 'Which employee\'s role would you like to change?',
+      choices: updatedEmp
+    },
+    {
+      type: 'list',
+      name: 'roleId',
+      message: 'What is the employee\'s new role?',
+      choices: selectRole
+    }
+  ])
+    .then(function (answer) {
+
+      const query =
+        `UPDATE employee SET role_id ? WHERE id = ?`
+        
+      db.query(query,
+        [answer.roleID, answer.empId],
+        function (error, results) {
+          if (error) throw error;
+          console.table(results);
+          console.log('Added! All set!');
+
+          startPrompts();
+        });
+    });
+}
 startPrompts();
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
